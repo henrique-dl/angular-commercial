ae7302a68b99e604094981e94644d352d61e61ea
Henrique <henrique.leite@unyleya.com.br>
2023-01-23 22:00:05 -0300

    Addin login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [LoginComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent],
 })
-export class AppModule { }
+export class AppModule {}
